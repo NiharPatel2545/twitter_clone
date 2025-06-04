b12bac664a36481a5c16a3926d4dea4a89feee0f
nihar patel <niharpatel2545@gmail.com>
2025-06-04 19:17:00 +0530

    updated code-4/6

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,30 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
-    res.json({
-      data: "You hit the singup endpoint",
-    });
+    try {
+      const { fullName, username, email, password } = req.body;
+
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+      if(!emailRegex.test(email)){
+        return res.status(400).json({ error: "Ivalid email format" });
+      }
+
+      const existingUser = await User.findOne({ username });
+      if(existingUser){
+        return res.status(400).json({ error: "Userame is already taken" })
+      }
+
+      const existingEmail = await User.findOne({ email });
+      if(existingEmail){
+        return res.status(400).json({ error: "Email is already taken" })
+      }
+
+      const salt = await bcrypt.genSalt(10);
+
+    } catch (error) {
+      
+    }
 }
 
 export const login = async (req, res) => {
@@ -15,3 +38,5 @@ export const logout = async (req, res) => {
       data: "You hit the logout endpoint",
     });
 }
+
+//2:53:06
