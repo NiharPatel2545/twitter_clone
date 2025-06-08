37c9e0eaec7db8ea682485b3890f414190a4906c
nihar patel <niharpatel2545@gmail.com>
2025-06-08 14:27:19 +0530

    incorporate logout and middleware functionality

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
-const router = express.Router();
 import { signup, login, logout } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
+import { getMe } from "../controllers/auth.controller.js";
 
+const router = express.Router();
+
+router.get("/me", protectRoute, getMe);
 router.post("/signup", signup);
-
 router.post("/login", login);
-
 router.post("/logout", logout);
 
 export default router;
