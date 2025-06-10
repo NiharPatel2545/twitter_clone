8c59a3d3ba4efd354f5952969d126ec12ff2a86f
nihar patel <niharpatel2545@gmail.com>
2025-06-10 12:28:11 +0530

    fix bugs related to notification functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Notification from "../models/notification.model.js";
 
 export const getUserProfile = async (req, res) => {
     const {username} = req.params;
@@ -34,22 +35,21 @@ export const followUnfollowUser = async (req, res) => {
             await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
             res.status(200).json({message: "User unfollowed successfully"});
 
-        } else{
+        } else {
 
             await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
-            await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+			await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
 
             const newNotification = new Notification({
-                type: "follow",
-                from: req.user._id,
-                to: userToModify._id
-            });
+				type: "follow",
+				from: req.user._id,
+				to: userToModify._id,
+			});
 
-            await newNotification.save();
+			await newNotification.save();
 
-            res.status(200).json({message: "User followed successfully"});
-
-        }
+			res.status(200).json({ message: "User followed successfully" });
+		}
     } catch (error) {
         console.log("Error in getUserProfile controller", error.message);
         res.status(500).json({error: error.message});
--- backend/models/notification.model.js
+++ backend/models/notification.model.js
@@ -1,26 +1,30 @@
-import mongoose from 'mongoose';
-const notificationSchema = new mongoose.Schema({
-    from:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    to:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    type:{
-        type: String,
-        required: true,
-        enum: ['follow', 'like']
-    },
-    read:{
-        type: Boolean,
-        default: falses
-    }
-}, {timestamps: true});
+import mongoose from "mongoose";
 
-const Notification = mongoose.model('Notification', notificationSchema);
+const notificationSchema = new mongoose.Schema(
+	{
+		from: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		to: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		type: {
+			type: String,
+			required: true,
+			enum: ["follow", "like"],
+		},
+		read: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ timestamps: true }
+);
+
+const Notification = mongoose.model("Notification", notificationSchema);
 
 export default Notification;
