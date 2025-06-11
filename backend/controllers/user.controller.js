420cd8ac019ecb73839e5950f24183c901619f24
nihar patel <niharpatel2545@gmail.com>
2025-06-11 13:23:13 +0530

    finish suggested users functionality and begin update user functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -82,3 +82,23 @@ export const getSuggestedUsers = async (req, res) => {
 		res.status(500).json({ error: error.message });
 	}
 };
+
+export const updateUser = async (req, res) => {
+    const { fullName, username, email, currentPassword, newPassword, bio, link } = req.body;
+    let {profileImg, coverImg} = req.body;
+
+    const userId = req.user._id;
+
+    try {
+        
+        const user = await User.findById(userId);
+        if(!user) return res.status(404).json({message: "User not found"});
+
+        if(!newPassword && !profileImg && !coverImg) {
+            return res.status(400).json({message: "No changes made"});
+        }
+
+    } catch (error) {
+        
+    }
+}
