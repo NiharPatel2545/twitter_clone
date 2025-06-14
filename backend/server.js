3e6a7498a48854c1889c4bc12e59752305a33535
nihar patel <niharpatel2545@gmail.com>
2025-06-14 14:21:53 +0530

    added new images and final changes.

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
+
 const app = express();
 
 dotenv.config();
@@ -19,8 +20,8 @@ cloudinary.config({
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
