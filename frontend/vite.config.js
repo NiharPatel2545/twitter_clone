075593875fa81e34a3377e94dc31c11ba943782b
nihar patel <niharpatel2545@gmail.com>
2025-06-14 21:24:25 +0530

    major updates-2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		port: 5173,
+		port: 3000,
 		proxy: {
-      "/api": "http://localhost:3000",
-    },
+			"/api": {
+				target: "http://localhost:5000",
+				changeOrigin: true,
+			},
+		},
 	},
 });
