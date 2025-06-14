171bfe005da2656a3b07456889dcac1830b24257
nihar patel <niharpatel2545@gmail.com>
2025-06-14 21:57:01 +0530

    minor changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 5173,
+    proxy: {
+      "/api": "http://localhost:3000",
+    },
   },
-})
+  preview: {
+    port: 5000, // This must match the PORT from Render
+    host: true, // Enables binding to 0.0.0.0
+    allowedHosts: ["twitter-clone-i2yb.onrender.com"], // 👈 Add your Render domain here
+  },
+});
