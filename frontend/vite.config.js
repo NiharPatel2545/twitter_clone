5744a3eb06b74cfab785809a0003e60ff474cf10
nihar patel <niharpatel2545@gmail.com>
2025-06-14 21:55:54 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000", // backend during dev
-        changeOrigin: true,
-      },
-    },
   },
-  preview: {
-    port: 5000,
-    host: true,
-    allowedHosts: "all", // allows Render preview or any external host
-  },
-});
+})
