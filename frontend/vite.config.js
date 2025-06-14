e0a8edd6a6ebd90589c0ed046154ddc05b40d96b
nihar patel <niharpatel2545@gmail.com>
2025-06-14 20:17:47 +0530

    fix: allow Render domain in Vite preview config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 5173,
-		proxy: {
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
       "/api": "http://localhost:3000",
     },
-	},
+  },
+  preview: {
+    port: 5000, // This must match the PORT from Render
+    host: true, // Enables binding to 0.0.0.0
+    allowedHosts: ["twitter-clone-i2yb.onrender.com"], // 👈 Add your Render domain here
+  },
 });
